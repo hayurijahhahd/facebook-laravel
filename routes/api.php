8de8bc35941eb625751ecb8c9f8a823854e7d1e5
Einnlleinhatt <hayurijahhahd@gmail.com>
2020-01-01 01:11:19 +0900

    User Profile Test

--- routes/api.php
+++ routes/api.php
@@ -15,10 +15,8 @@ use Illuminate\Http\Request;
 
 Route::middleware('auth:api')->group(function () {
 
-    Route::get('/user', function (Request $request) {
-        return $request->user();
-    });
-
-    Route::get('/posts', 'PostController@index');
-    Route::post('/posts', 'PostController@store');
+    Route::apiResources([
+        'posts' => 'PostController',
+        'users' => 'UserController'
+    ]);
 });
